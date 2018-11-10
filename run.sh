c0bea2165fffbbeb292f3673c47a0baeac0a9a5d
Mehdi Bounya <contact.mehdi@pm.me>
2018-11-10 12:48:31 +0000

    Sileeent!

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mkdir -p /tmp/spoody/
 
-curl -L https://github.com/mehdibo/golden-cable/blob/master/pranky-darwin-x64.tar.gz?raw=true -o /tmp/spoody/pranky.tar.gz > /dev/null
+curl -s -L https://github.com/mehdibo/golden-cable/blob/master/pranky-darwin-x64.tar.gz?raw=true -o /tmp/spoody/pranky.tar.gz > /dev/null
 
 tar -xvf /tmp/spoody/pranky.tar.gz -C /tmp/spoody > /dev/null
 
